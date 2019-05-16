38be0520e18bdd8f48e8aaaf7dbb892fd42620c9
Ken Zheng <pengfeizheng@hotmail.com>
2019-05-17 00:40:21 +0800

    20190517

--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,18 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(
+    vscode.commands.registerCommand('catCoding.start', () => {
+      // Create and show a new webview
+      const panel = vscode.window.createWebviewPanel(
+        'catCoding', // Identifies the type of the webview. Used internally
+        'Cat Coding', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
